e99083cc2fc2002e9984491c09c0a54fb55c5531
Simon Walker <github@helpmebot.org.uk>
2012-01-18 04:08:09 +0000

    tidy up, take out s3 code

--- updateWiki.php
+++ updateWiki.php
@@ -1,42 +1,24 @@
 <?php
+// initial configuration
+$settings = array();
 
-$url = "https://s3-eu-west-1.amazonaws.com/minecraft-worlds/smp/mc1/";
+$settings['username']="Stwalkerster";
+$settings['password']=""; // get from ext. file not in git.
+
 /*$pw = file_get_contents*/require_once("wiki.password");
-$un = "Minecraftbackupscript";
-$page = "Minecraft Backups";
 
-
-$text = "This is a list of all the available world backups for the \"MC1\" server:\n\n";
-
-$result = shell_exec("s3cmd ls s3://minecraft-worlds/smp/mc1/2");
-$result = explode("\n", $result);
-$result = array_filter($result);
-$data = array();
-foreach($result as $val)
-{
-	$arrayval = explode(" " ,$val);
-	unset($arrayval[0]);
-	unset($arrayval[1]);
-	$arrayval[6] = str_replace("s3://minecraft-worlds/smp/mc1/", "", $arrayval[6]);
-	$arrayval[6] = str_replace(".tar.bz2", "", $arrayval[6]);
-	$d = DateTime::createFromFormat("Y-m-d\THis\Z", $arrayval[6]);
-	$arrayval[] = $d->format("l d F Y H:i:s");
-	$arrayval = array_filter($arrayval);
-	$arrayval =array_values( $arrayval);
-	$data[] = $arrayval;
-	$size = ($arrayval[0] / 1024) / 1024;
-	$text .= '* '.$arrayval[2].' : ['.$url.$arrayval[1].'.tar.bz2 Download] ('.number_format($size,2)." MB)\n";
-}
-
+	
+	
 $api = "http://helpmebot.org.uk/w/api.php";
 $settings['cookiefile'] = "cookies.tmp";
+$settings['useragent'] = 'BlockerBot/1.0 ( +http://github.com/stwalkerster/blockerbot ) cURL/php';
 
 function httpRequest($url, $post="") {
         global $settings;
 
         $ch = curl_init();
         //Change the user agent below suitably
-        curl_setopt($ch, CURLOPT_USERAGENT, 'MinecraftBackupScript/1.0 cURL/PHP');
+        curl_setopt($ch, CURLOPT_USERAGENT, $settings['useragent']);
         curl_setopt($ch, CURLOPT_URL, ($url));
         curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -56,9 +38,9 @@ function httpRequest($url, $post="") {
 $apiresult = httpRequest($api, array(
 	"format" => "php",
 	"action" => "login",
-	"lgname" => $un,
-	"lgpassword" => $pw,
-	"lgdomain" => "Helpmebot SSO",
+	"lgname" => $settings['username'],
+	"lgpassword" => $settings['password'],
+	"lgdomain" => "Helpmebot SSO" ,//////////////////////////////////////////////////////////////////////////
 	));
 
 $apiresult = unserialize($apiresult);
@@ -70,8 +52,8 @@ if($apiresult["login"]["result"] != "NeedToken")
 $apiresult = httpRequest($api, array(
 	"format" => "php",
 	"action" => "login",
-	"lgname" => $un,
-	"lgpassword" => $pw,
+	"lgname" => $settings['username'],
+	"lgpassword" => $settings['password'],
 	"lgdomain" => "Helpmebot SSO",
 	"lgtoken" => $apiresult["login"]["token"],
 	));
@@ -82,14 +64,14 @@ echo "########## Login part 2:\n";
 if($apiresult["login"]["result"] != "Success")
 	die( "Login: {$apiresult["login"]["result"]}");
 
-/////////////// GET EDIT TOKEN
+/////////////////////////////// GET EDIT TOKEN
 
 $apiresult = httpRequest($api, array(
 	"format" => "php",
 	"action" => "query",
 	"prop" => "info",
 	"intoken" => "edit",
-	"titles" => $page,
+//	"titles" => $page,
 	));
 
 $apiresult = unserialize($apiresult);
@@ -112,7 +94,7 @@ $apiresult = httpRequest($api, array(
 	"token" => $token,
 	"summary" => "Update data from Minecraft Backup script",
 	"text" => $text,
-	"title" => $page
+//	"title" => $page
 	));
 
 $apiresult = unserialize($apiresult);
